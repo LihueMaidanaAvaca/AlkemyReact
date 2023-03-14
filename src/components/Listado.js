0a579e43fe1e3b5b628d8616caeaa5f9f4cfc383
Lihue Maidana Avaca <lihuevida@gmail.com>
2023-03-14 17:18:39 -0300

    token de seguridad

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
+
 export default function Listado() {
+
+    const history = useNavigate()
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if(token === null){
+           history('/');  
+        }
+      },[]);
+
+
   return (
     <>
       <h2>Componente Listado</h2>
